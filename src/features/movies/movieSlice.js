f9fd53c004412d1f7e52b0429f57724edeedeac5
JuanCoiroORT <juancoiroelizalde@gmail.com>
2026-02-24 19:07:49 -0300

    Se empezo thunk peliculas

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,24 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchMovies = createAsyncThunk(
+  "movies/fetchMovies",
+  async (_, { getState, rejectWithValue }) => {
+    try {
+      const token = getState().auth.token;
+
+      const response = await fetch(
+        "https://movielist.develotion.com/peliculas",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      const data = await response.json();
+
+      if (!response.ok) {
+        return rejectWithValue(data.mensaje);
+      }
+
+      return data.peliculas;
+    } catch (error) {
+      return rejectWithValue("Error al obtener películas");
+    }
+  },
+);
 
 const initialState = {
-    list: [],
-    filter: "all"
+  list: [],
+  loading: false,
+  error: null,
+  filter: "all",
 };
 
 const movieSlice = createSlice({
-    name: "movies",
-    initialState,
-    reducers: {
-        addMovie: (state, action) => {
-            state.list.push(action.payload);
-        },
-        deleteMovie: (state, action) => {
-            state.list = state.list.filter(movie => movie.id !== action.payload);
-        },
-        setFilter: (state, action) => {
-            state.filter = action.payload;
-        }
-    }
+  name: "movies",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+        .addCase(fetchMovies.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(fetchMovies.fulfilled, (state, action) => {
+            state.loading = false;
+            state.list = action.payload;
+        })
+        .addCase(fetchMovies.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        })
+  },
 });
 
 export const { addMovie, deleteMovie, setFilter } = movieSlice.actions;
